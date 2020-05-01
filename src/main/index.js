516a77e270cb7f485086ab0b2443398814a01198
Arnaldo Capo <arnaldo@fatmap.com>
2020-05-01 15:49:51 +0200

    Added the ability to add currencies. Added tezos as currency and set it to default

--- src/main/index.js
+++ src/main/index.js
@@ -1,60 +1,64 @@
-'use strict'
+"use strict";
 
-import { app, BrowserWindow, Tray, Menu } from 'electron'
-import path from 'path'
-import axios from 'axios'
+import { app, BrowserWindow, Tray, Menu } from "electron";
+import path from "path";
+import axios from "axios";
 
 /**
  * Set `__static` path to static files in production
  * https://simulatedgreg.gitbooks.io/electron-vue/content/en/using-static-assets.html
  */
-if (process.env.NODE_ENV !== 'development') {
-  global.__static = require('path').join(__dirname, '/static').replace(/\\/g, '\\\\')
+if (process.env.NODE_ENV !== "development") {
+  global.__static = require("path")
+    .join(__dirname, "/static")
+    .replace(/\\/g, "\\\\");
 }
 
-let mainWindow
-let tray
+let mainWindow;
+let tray;
+let selectedCurrency;
 
-const winURL = process.env.NODE_ENV === 'development'
-  ? `http://localhost:9080`
-  : `file://${__dirname}/index.html`
+const winURL =
+  process.env.NODE_ENV === "development"
+    ? `http://localhost:9080`
+    : `file://${__dirname}/index.html`;
 
-function createWindow () {
+function createWindow() {
   /**
    * Initial window options
    */
   mainWindow = new BrowserWindow({
     height: 563,
     useContentSize: true,
-    width: 1000
-  })
+    width: 1000,
+  });
 
-  mainWindow.loadURL(winURL)
+  mainWindow.loadURL(winURL);
 
-  mainWindow.on('closed', () => {
-    mainWindow = null
-  })
+  mainWindow.on("closed", () => {
+    mainWindow = null;
+  });
 }
 
-app.on('ready', () => {
-  createTray()
-  startTick()
-  fetchPrice()
-})
+app.on("ready", () => {
+  createTray();
+  startTick();
+  fetchPrice();
+});
 
-app.on('window-all-closed', () => {
-  if (process.platform !== 'darwin') {
-    app.quit()
+app.on("window-all-closed", () => {
+  if (process.platform !== "darwin") {
+    app.quit();
   }
-})
+});
 
-app.on('activate', () => {
-  console.log('Activating')
+app.on("activate", () => {
+  console.log("Activating");
   if (mainWindow === null) {
-    console.log('creating window')
-    createWindow()
+    console.log("creating window");
+    createWindow();
   }
-})
+});
 
 // const toggleWindow = () => {
 //   if (mainWindow.isVisible()) {
@@ -65,56 +69,84 @@ app.on('activate', () => {
 //   }
 // }
 
+const currencies = {
+  eth: {
+    image: "eth.png",
+    gdaxId: "ETH-USD",
+  },
+  xtz: {
+    image: "tezos.png",
+    gdaxId: "XTZ-USD",
+  },
+};
+
+selectedCurrency = currencies["xtz"];
+
+const selectCurrency = (currencyId) => {
+  selectedCurrency = currencies[currencyId];
+  fetchPrice();
+  tray.setImage(path.join(__static, selectedCurrency.image));
+};
+
 const createTray = () => {
-  tray = new Tray(path.join(__static, 'eth.png'))
-  tray.setTitle('--')
+  tray = new Tray(path.join(__static, selectedCurrency.image));
+  tray.setTitle("--");
 
   const contextMenu = Menu.buildFromTemplate([
     {
-      label: 'Alerts',
-      type: 'normal',
+      label: "ETH",
+      type: "normal",
       click: () => {
-        // mainWindow.show()
-        // mainWindow.focus()
-        createWindow()
-      }
+        selectCurrency("eth");
+      },
     },
     {
-      label: 'Quit',
-      type: 'normal',
+      label: "XTZ",
+      type: "normal",
       click: () => {
-        app.quit()
-      }
-    }
-  ])
+        selectCurrency("xtz");
+      },
+    },
+    {
+      label: "Quit",
+      type: "normal",
+      click: () => {
+        app.quit();
+      },
+    },
+  ]);
 
-  tray.setContextMenu(contextMenu)
-  tray.on('click', function (event) {
+  tray.setContextMenu(contextMenu);
+  tray.on("click", function (event) {});
+};
 
-  })
-}
+const fetchPrice = async () => {
+  console.log("fetching Price", selectedCurrency);
 
-const fetchPrice = () => {
-  axios.get('https://api.gdax.com/products/ETH-USD/ticker')
-    .then(response => {
-      const body = response.data
-      var price = parseFloat(body.price)
+  const { gdaxId } = selectedCurrency;
 
-      if (isNaN(price)) {
-        return
-      }
+  const response = await axios.get(
+    `https://api.gdax.com/products/${gdaxId}/ticker`
+  );
 
-      price = price.toFixed(2)
-      tray.setTitle('$' + price)
-    })
-    // .then(function(){
-    //   tray.setHighlightMode('never')
-    // })
-}
+  const body = response.data;
+  var price = parseFloat(body.price);
+
+  if (isNaN(price)) {
+    return;
+  }
+
+  price = price.toFixed(2);
+  tray.setTitle("$" + price);
+
+  // .then(function(){
+  //   tray.setHighlightMode('never')
+  // })
+};
 
 const startTick = () => {
-  setInterval(fetchPrice, 60000)
-}
+  setInterval(fetchPrice, 60000);
+};
 
 /**
  * Auto Updater
